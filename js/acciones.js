97144b5e9fca01127aeed4fd5bc4428f9590c63a
ReneCervera <cb16.cervera.flores.jose@gmail.com>
2019-05-07 19:32:08 -0500

    1subida

---
+++ js/acciones.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+
+	$('#Izquierda').on("swipeleft",function(){
+	navigator .notification. alert("Deslizo a la Izquierda",function(){"aplicacion ","aceptar"});
+	});
+
+$('#derecha').on("swiperight",function(){
+	navigator .notification. confirm("¿Que quieres hacer?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.beep(1);
+			break;
+
+			case 2:
+			navigator.notification.vibrate(1000);
+			break;
+		}
+	},"aplicacion ","beep,vibrar,cancelar");
+
+	});	
+
+	
+	},false);
+	
+});//document
+		
